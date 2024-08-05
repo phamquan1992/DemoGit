5b99f73d50d091d4d16d3d011198912370fc2b06
phamquan1992 <phamquan2471992@gmail.com>
2024-08-05 23:20:22 +0700

    test lan 3

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGit.Models
+{
+    public class Product
+    {
+        public required string name { get; set; }
+        public string? description { get; set; }
+        public int id { get; set; }
+    }
+}
